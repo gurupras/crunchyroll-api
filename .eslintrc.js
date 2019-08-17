d7fed1cc24c616a8c4ef5dc8915b4fe0e03d5546
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2019-08-17 17:52:33 -0400

    package fixup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,20 @@
 module.exports = {
-    "extends": [
-      "standard",
-      "plugin:jest/recommended"
-    ]
-};
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: [
+    'standard',
+    'plugin:jest/recommended'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  rules: {
+  }
+}

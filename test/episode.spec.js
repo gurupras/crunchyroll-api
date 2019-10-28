07ef4bf61e5c2fdffe929a0947b7663da02edcd3
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2019-10-28 13:36:59 -0400

    Uncommented test for isPremiumVideo()

--- test/episode.spec.js
+++ test/episode.spec.js
@@ -1,4 +1,6 @@
+import axios from 'axios'
 import Episode from '@/episode'
+import jsdom from 'jsdom'
 
 let episode
 beforeEach(() => {
@@ -14,11 +16,22 @@ describe('Episode', () => {
     expect(episode.episodeNumber).toEqual(1)
   })
 
-  test.skip('isPremiumVideo', async () => {
+  test('isPremiumVideo', async () => {
     // TODO: Will have to change this URL on every  test
-    episode = new Episode('https://www.crunchyroll.com/dr-stone/episode-12-buddies-back-to-back-786952')
+    await expect(episode.isPremiumVideo()).resolves.toBe(false)
+    // Now load a premium video
+    // We get a known-premium-video by scraping crunchyroll's home page for a premium video
+    const response = await axios.get('https://www.crunchyroll.com')
+    const { data: page } = response
+    const dom = new jsdom.JSDOM(page)
+    const premiumAnchorImage = dom.window.document.body.querySelector('img[src="https://www.crunchyroll.com/i/premium_crown_tiny.png"]')
+    expect(premiumAnchorImage).toBeTruthy()
+    const premiumAnchor = premiumAnchorImage.parentNode
+    expect(premiumAnchor).toBeTruthy()
+    const url = `https://www.crunchyroll.com/${premiumAnchor.href}`
+    episode = new Episode(url)
     await expect(episode.isPremiumVideo()).resolves.toBe(true)
-  })
+  }, 300000)
 
   test('Subtitle conversion', async () => {
     await episode.parse()
